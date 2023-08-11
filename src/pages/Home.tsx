b2fc1f73be916cf6521baaac46b97be39f70789a
annmonrajijames <annmonjameschiramel@gmail.com>
2023-08-11 13:58:33 +0530

    second commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+ 
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Ionic Login Page </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+     <IonContent className="ion-padding">
+      <IonButton routerLink="/login" color="primary">Login</IonButton>
+      <IonButton routerLink="/register" color="secondary">Register</IonButton>
+      <IonButton routerLink="/login1" color="secondary">Login1</IonButton>
       </IonContent>
+  
     </IonPage>
   );
 };
